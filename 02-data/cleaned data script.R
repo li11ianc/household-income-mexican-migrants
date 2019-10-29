a76f34b9ccae2f900a4f90fe10a9d946793e8281
Qintian Zhang <qintian.zhang@duke.edu>
2019-10-29 16:01:08 -0400

    data clean

--- 02-data/cleaned data script.R
+++ 02-data/cleaned data script.R
@@ -4,16 +4,10 @@ library(tidyverse)
 
 pers170 <- read_csv(paste0("/Users/qintianzhang/Downloads/pers170.csv"))
 imm <- pers170 %>%
-  select(sex, relhead, yrborn, age, statebrn, marstat, edyrs, occ, hhincome, ldowage, health, healthly, healthnw)
-imm %>%
-  filter(hhincome != 9999, hhincome != 8888)
-imm %>%
-  filter(!is.na(health))
-imm %>%
-  filter(!is.na(hhincome))
-imm %>%
-  filter(!is.na(healthy))
-imm %>%
-  filter(!is.na(healthnw))
-imm %>%
-  filter(!is.na(ldowage))
+  select(sex, relhead, yrborn, age, statebrn, marstat, edyrs, occ, hhincome, usstate1, usstatel, usplace1, usplacel, usdur1, usdurl, usdur1)%>%
+  filter(hhincome != 9999 ,
+         usstate1 != 9999,
+         usstatel != 9999,
+         usdur1 != 9999,
+         usdurl != 9999)
+imm
